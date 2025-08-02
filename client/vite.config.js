29e164702ed661b0e3098bf535e63846197d44dd
ghf4336 <adrianliow@hotmail.com>
2025-08-02 21:24:50 +1200

    update to use github pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PaddleStack/', // <-- Add this line
+  base: '/PaddleStack/', // Ensures correct asset paths for GitHub Pages
   plugins: [react()],
 });
